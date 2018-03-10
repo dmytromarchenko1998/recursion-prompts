1a60b14f90bc642a0c326dd3209b2d6ca7554c20
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-03-09 22:33:36 -0800

    got third test to pass

--- src/recursion.js
+++ src/recursion.js
@@ -46,6 +46,17 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var newArray = array.slice();
+  if (newArray.length === 0) {
+  	return 0;
+  }
+  else {
+  	var lastVal = newArray.pop();
+  	if (typeof lastVal !== 'number') {
+  	  lastVal = arraySum(lastVal);
+  	}
+  	return (lastVal + arraySum(newArray))
+  }
 };
 
 // 4. Check if a number is even.
